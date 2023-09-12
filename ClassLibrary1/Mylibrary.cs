8881d1be9ef8750167e6339ee70138e9787d6e66
TomasArno <arnotomas1@gmail.com>
2023-09-12 16:15:33 -0300

    Add project files.

---
+++ ClassLibrary1/Mylibrary.cs
@@ -0,0 +1,31 @@
+﻿namespace MyLibrary
+{
+    public class Sumador
+    {
+        int _cantidadSumas;
+        public Sumador():this(0)
+        {
+        }
+        public Sumador(int cantidadSumas)
+        {
+            this._cantidadSumas=cantidadSumas;
+        }
+
+        public long Sumar(long num1, long num2)
+        {
+            this._cantidadSumas++;
+            return num1 + num2;
+        }
+
+        public string Sumar(string text1, string text2)
+        {
+            this._cantidadSumas++;
+            return text1 + text2;
+        }
+
+        public static explicit operator int(Sumador sumador)
+        {
+            return sumador._cantidadSumas;
+        }
+    }
+}
---
+++ ConsoleAppMain/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyLibrary;
+
+namespace ConsoleAppMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int conver1 = 0;
+            int conver2 = 0;
+
+            Sumador mySumador1 = new();
+            Sumador mySumador2 = new Sumador(5);
+
+            long resultadoLong = mySumador1.Sumar(1, 1);
+            Console.WriteLine(resultadoLong);
+
+            string resultadoStr = mySumador1.Sumar("12", " hola");
+            Console.WriteLine(resultadoStr);
+
+            conver1 = (int)mySumador1;
+            conver2 = (int)mySumador2;
+
+            Console.WriteLine(conver1);
+            Console.WriteLine(conver2);
+
+        }
+    }
+}
